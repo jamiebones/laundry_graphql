9df61ed1626712236a78d45313786fe7c76cbbb4
jamiebones <jamiebones2000@yahoo.co.uk>
2020-06-21 13:25:23 +0100

    first deploy 3

--- server/app.js
+++ server/app.js
@@ -61,6 +61,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
+const port = process.env.PORT || 4000;
+
+app.listen(port , () => {
+  console.log(`app listening on port ${port}`);
 });
