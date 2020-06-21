72c020323cadbaf11794fb7b16d7f37061dcccb9
jamiebones <jamiebones2000@yahoo.co.uk>
2020-06-21 13:06:23 +0100

    first deploy

--- server/app.js
+++ server/app.js
@@ -54,6 +54,7 @@ app.use("/graphql", (req, res ) =>
 );
 
 if (process.env.NODE_ENV === "production") {
+  console.log("production mode")
   app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/", "index.html"));
